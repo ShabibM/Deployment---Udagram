ebc9300a82a824770c44ae18aeeab61dd4633ce9
Shabib Mohammed <uniqueb7h@gmail.com>
2023-02-13 18:51:34 +0300

    env

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,5 @@
+export NODE_OPTIONS=--openssl-legacy-provider
+
 aws s3 cp --recursive --acl public-read ./www s3://mynewbuckethere/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mynewbuckethere/
 
