29ecc4776377c708839b152ea9fc6e77952758bb
Shabib Mohammed <uniqueb7h@gmail.com>
2023-02-13 17:48:01 +0300

    Update set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -4,6 +4,7 @@ export POSTGRES_USERNAME=admin123
 export POSTGRES_PASSWORD=admin123
 export POSTGRES_HOST=udagram.crazbvbub8hw.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
+export AWS_SECRET_ACCESS_KEY=cWfIvMwZ4cXNtOrHMBDpReVWnM76Ro1Nr1pRV73V
 export AWS_BUCKET=arn:aws:s3:::mynewbuckethere
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
